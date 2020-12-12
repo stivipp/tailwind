905cfe59a20957a24d6259d858de9c6a8568ae75
stivipp <stivippx@gmail.com>
2020-12-12 22:14:27 +0100

    Hram sa s COLORS a tailwind.config.js file kde pridavam nove farbicky :D

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '420px',
+      },
+      colors: {
+        teal: {
+          '100' : '#E6FFFA',
+          '200' : '#B2F5EA',
+          '300' : '#81E6D9',
+          '400' : '#4FD1C5',
+          '500' : '#38B2AC',
+          '600' : '#319795',
+          '700' : '#2C7A7B',
+          '800' : '#285E61',
+          '900' : '#234E52',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
